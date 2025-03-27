f2f61b639c8cc01d22e3705fe4d226a8143d7d9c
davidsalgadoo <davidguilherme12345@gmail.com>
2025-03-27 20:12:05 -0300

    contador

--- android/app/src/main/kotlin/com/example/flutter_app_estado/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_app_estado
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,14 +16,39 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void decrementar() {
+    setState(() {
+      contador--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: Text('$contador', style: TextStyle(fontSize: 40))),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementar,
-        backgroundColor: Colors.blue,
-        child: Icon(Icons.add, color: Colors.white, size: 30),
+      floatingActionButton: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Positioned(
+            left: 100,
+            bottom: 20,
+            child: FloatingActionButton(
+              onPressed: decrementar,
+              backgroundColor: Colors.red,
+              child: Icon(Icons.remove, color: Colors.white, size: 30),
+            ),
+          ),
+
+          Positioned(
+            right: 100,
+            bottom: 20,
+            child: FloatingActionButton(
+              onPressed: incrementar,
+              backgroundColor: Colors.blue,
+              child: Icon(Icons.add, color: Colors.white, size: 30),
+            ),
+          ),
+        ],
       ),
     );
   }
